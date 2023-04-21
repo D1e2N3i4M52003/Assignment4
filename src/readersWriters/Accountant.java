359e6827a28ccbd7ba520951d64c78576494a33d
gramovi4a <331957@viauc.dk>
2023-04-21 15:40:57 +0200

    readerswriters

--- src/readersWriters/Accountant.java
+++ src/readersWriters/Accountant.java
@@ -2,9 +2,27 @@ package readersWriters;
 
 public class Accountant implements Runnable{ // Accountant is a reader
 
-
+	private Door door;
+	public Accountant(Door door) {
+		this.door = door;
+	}
 	@Override
 	public void run() {
-
+		while(true) {
+			EnterAccess list = door.acquireRead();
+			list.count();
+			LogLine logLine = new LogLine(list.count().toString());
+			Log log = Log.getInstance();
+			log.addToFile(logLine);
+			door.releaseRead();
+			try
+			{
+				Thread.sleep(10000);
+			}
+			catch (InterruptedException e)
+			{
+				throw new RuntimeException(e);
+			}
+		}
 	}
 }
